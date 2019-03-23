78f05753373b85d19a2d7b6fd7e29c04dcea60b8
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-03-22 21:35:14 -0400

    BRAAAAAAAAAAD

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,23 +2,20 @@ var seriousBtn = [
     {   name: "nebula",
         src: "assets/images/nebula.png",
     },
-    {   name: "moon phases",
-        src: "assets/images/moon-png-44667.png",
+    {   name: "star trek space",
+        src: "assets/images/trek.png",
     },
-    {   name: "saturn",
+    {   name: "planets",
         src: "assets/images/saturn_dan_gerhards_01.svg",
     },
+    {   name: "pizza in space",
+        src: "assets/images/pizza.png",
+    },
+    {   name: "lumpy space princess",
+        src: "assets/images/lumpy.png",
+    },    
     {   name: "comet",
         src: "assets/images/comet.png",
-    },
-    {   name: "astronaut",
-        src: "assets/images/astro.png",
-    },
-    {   name: "alien",
-        src: "assets/images/alien.png",
-    },    
-    {   name: "mars",
-        src: "assets/images/mars.png",
     }, 
 ];
 
@@ -44,7 +41,7 @@ $(".btn-container").on('click', ".gif-button", function() {
 
     var name = $(this).attr("data-name");
     console.log(name);
-    var queryUrl = `https://api.giphy.com/v1/gifs/search?api_key=zVyDwVspu8j5Z5ZC0wZCfki71mHSaYur&q=${name}&limit=10&offset=0&rating=PG-13&lang=en`
+    var queryUrl = `https://api.giphy.com/v1/gifs/search?api_key=zVyDwVspu8j5Z5ZC0wZCfki71mHSaYur&q=${name}&limit=12&offset=0&rating=PG-13&lang=en`
 
     $.ajax({
         url: queryUrl,
@@ -53,29 +50,41 @@ $(".btn-container").on('click', ".gif-button", function() {
         console.log(response);
         var gifs = response.data
         for(i=0; i < gifs.length; i++) {
+            var rating = gifs[i].rating;
             var gif = $(`
-                <img class="gifs" 
+                <img class="gifs img-fluid" 
                 alt="${name}-gif-${i}" 
-                src="${gifs[i].images.original_still.url}"
-                data-still="${gifs[i].images.original.url}"
-                data-animate="${gifs[i].images.original_still.url}"
+                src="${gifs[i].images.fixed_height_still.url}"
+                data-still="${gifs[i].images.fixed_height_still.url}"
+                data-animate="${gifs[i].images.fixed_height.url}"
                 data-state="still"
             >`);
-            $("#display").prepend(gif);
+            var p = $("<p>").text(`Rating: ${rating}`) ;
+            var d = $("<div>").addClass("gif-house");
+            d.addClass("col-md-3")
+            d.append(gif);
+            d.append(p);
+            $("#display").prepend(d);
         }
     })
 });
 
-//come back to this
-// $("#user-input").on('keyup', function(event)  {
-//     if (event.keyCode == 13) {
-//         event.preventDefault();
-//         $('#input-btn').click()
-//     }
-// });
+$("#display").on("click", ".gifs", function() {
+    console.log("I clicked a diff!")
+    var state = $(this).attr("data-state");
+    console.log(state);
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+});
+
 
 $("#input-btn").on('click', function(event) {
-    event.preventdefault();
+    event.preventDefault();
     var userInput = $("#user-input").val().trim()
     var userBtn = $(`<button>`)
     userBtn.text(`${userInput}`);
@@ -91,42 +100,4 @@ $("#clear").on("click", function() {
     $("#display").empty();
 
 });
-
-/* <img
-src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif"
-data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif"
-data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif"
-data-state="still"
-class="gif"
-/>
-<img
-src="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif"
-data-still="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif"
-data-animate="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200.gif"
-data-state="still"
-class="gif"
-/>
-<img
-src="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif"
-data-still="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif"
-data-animate="https://media3.giphy.com/media/W6LbnBigDe4ZG/200.gif"
-data-state="still"
-class="gif"
-/>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript">
-$(".gif").on("click", function() {
-  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-  var state = $(this).attr("data-state");
-  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-  // Then, set the image's data-state to animate
-  // Else set src to the data-still value
-  if (state === "still") {
-    $(this).attr("src", $(this).attr("data-animate"));
-    $(this).attr("data-state", "animate");
-  } else {
-    $(this).attr("src", $(this).attr("data-still"));
-    $(this).attr("data-state", "still");
-  }
-}); */
 
